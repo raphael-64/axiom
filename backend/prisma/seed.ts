964d7a717fa444d6ec84f25642d3f230b3379330
Rajan Agarwal <rajan.ag005@gmail.com>
2024-10-30 12:02:37 -0400

    seeding

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,15 +1,30 @@
-import prisma from '../src/prisma';
+import prisma from "../src/prisma";
 
 async function main() {
-  await prisma.user.create({
+  await prisma.user.deleteMany({});
+  await prisma.workspace.deleteMany({});
+
+  const user = await prisma.user.create({
     data: { id: "r34agarw" },
   });
 
+  const workspace = await prisma.workspace.create({
+    data: {
+      project: "Assignment 0",
+      users: {
+        connect: { id: user.id },
+      },
+    },
+    include: { users: true },
+  });
+
   console.log("Seeding completed");
+  console.log("User:", user);
+  console.log("Workspace with connected User:", workspace);
 }
 
 main()
-  .catch(e => console.error(e))
+  .catch((e) => console.error(e))
   .finally(async () => {
     await prisma.$disconnect();
   });
