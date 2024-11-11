a0b77af4b1ce437f0a279c7690c1fcb554f226c6
Ishaan Dey <69771365+ishaan1013@users.noreply.github.com>
2024-11-11 15:32:01 -0800

    Delete backend/dist directory

--- backend/dist/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-// src/utils.ts
-const greet = (name) => {
-    return `Hello, ${name}!`;
-};
-exports.greet = greet;
-// You can add more utility functions here
