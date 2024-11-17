69d41e2c4c78ba24026dee089565261a55350d1d
Ishaan Dey <ishaan1013@gmail.com>
2024-11-17 18:24:21 -0500

    change color picker library and ui

--- frontend/components/editor/colorPicker.tsx
+++ frontend/components/editor/colorPicker.tsx
@@ -1,58 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { ChromePicker } from "react-color";
+import { HexColorPicker } from "react-colorful";
 import { useColorTheme } from "@/components/providers/color-context";
+import "@/styles/color.css";
 
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@/components/ui/popover";
+import { Input } from "../ui/input";
 
 export default function ColorPicker({
-    token,
-    defaultColor,
+  token,
+  defaultColor,
 }: {
-    token: string;
-    defaultColor: string;
+  token: string;
+  defaultColor: string;
 }) {
-    const colorTheme = useColorTheme();
-    const updateColor = colorTheme ? colorTheme.updateColor : () => {};
-    const [color, setColor] = useState(defaultColor);
+  const colorTheme = useColorTheme();
+  const updateColor = colorTheme ? colorTheme.updateColor : () => {};
+  const [color, setColor] = useState(defaultColor);
 
-    useEffect(() => {
-        setColor(defaultColor);
-    }, [defaultColor]);
+  useEffect(() => {
+    setColor(defaultColor);
+  }, [defaultColor]);
 
-    const handleChange = (color: any) => {
-        setColor(color.hex);
-        updateColor(token, color.hex); // Update the correct theme (dark or light)
-    };
+  const handleChange = (color: string) => {
+    setColor(color);
+    updateColor(token, color);
+  };
 
-    return (
-        <>
-            <Popover>
-                <PopoverTrigger
-                    className="w-12 h-6 rounded-md"
-                    style={{ backgroundColor: color }}
-                ></PopoverTrigger>
-                <PopoverContent className="w-fit p-1" side="right">
-                    <div style={{ background: "red" }}>
-                        <ChromePicker
-                            color={color}
-                            onChange={handleChange}
-                            disableAlpha={true}
-                            styles={{
-                                default: {
-                                    body: {
-                                        fontFamily: "var(--font-geist-sans)",
-                                        fontSize: "12px",
-                                    },
-                                },
-                            }}
-                        />
-                    </div>
-                </PopoverContent>
-            </Popover>
-        </>
-    );
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger
+          className="w-12 h-6 rounded-md"
+          style={{ backgroundColor: color }}
+        ></PopoverTrigger>
+        <PopoverContent className="w-fit p-2" side="right">
+          <div className="color-picker">
+            <HexColorPicker color={color} onChange={handleChange} />
+          </div>
+          <div className="flex justify-between items-center mt-2">
+            <div className="text-sm">HEX</div>
+            <Input
+              type="text"
+              placeholder="#123ABC"
+              className="w-24 font-mono"
+              value={color}
+              onChange={(e) => handleChange(e.target.value)}
+            />
+          </div>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
 }
--- frontend/components/providers/index.tsx
+++ frontend/components/providers/index.tsx
@@ -2,25 +2,32 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { ColorThemeProvider } from "./color-context";
+import dynamic from "next/dynamic";
 import { Toaster } from "@/components/ui/sonner";
+
+const ColorThemeProvider = dynamic(
+  () => import("./color-context").then((mod) => mod.ColorThemeProvider),
+  {
+    ssr: false,
+  }
+);
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-            >
-                <ColorThemeProvider>
-                    {children}
-                    <Toaster />
-                </ColorThemeProvider>
-            </ThemeProvider>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ColorThemeProvider>
+          {children}
+          <Toaster />
+        </ColorThemeProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
